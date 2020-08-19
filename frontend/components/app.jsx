3dd482d7a9134b1fe8822652e6dbc8674e11403c
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-08-19 14:54:16 -0400

    Heroku push

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import { AuthRoute, ProtectedRoute, Redirect} from '../util/route_util';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute} from '../util/route_util';
 import SignupContainer from './session/signup_container'
 import LoginContainer from './session/login_container';
 import UserShowContainer from './user_show/user_show_container';
@@ -8,13 +8,21 @@ import UserShowContainer from './user_show/user_show_container';
 export default () => (
   <div>
 
-    <Switch>
+    <Switch> 
+
+    {/* Auth route for login */}
+
+    {/* Everything else protected */}
+
+
       <Route exact path='/' component={LoginContainer} />
       <Route path='/signup' component={SignupContainer} />
       <Route exact path='/users/:userId' component={UserShowContainer} />
       {/* <ProtectedRoute path="/users/" component={UserShowContainer} /> */}
 
-      {/* <Redirect from="*" to="/" /> */}
+      {/* Catch all redirect */}
+      {/* Either redirect to feed/profile page if logged in */}
+      <Redirect to="/" />
     </Switch>
   </div>
 )
