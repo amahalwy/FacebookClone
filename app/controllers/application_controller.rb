876c5b811cfe76b9e7180060b226c5e7dfa1379a
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-08-28 00:07:45 -0400

    Added protect from forgery back in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token #For postman usage
+  protect_from_forgery with: :null_session
+  # skip_before_action :verify_authenticity_token #For postman usage
 
   helper_method :current_user, :logged_in?
 
