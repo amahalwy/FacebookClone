e91de46361223a89314d8a33cf6001947f7e729c
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-10-07 20:35:33 -0400

    Friendship index fix

--- frontend/components/friendships/friendship_index_item.jsx
+++ frontend/components/friendships/friendship_index_item.jsx
@@ -6,7 +6,9 @@ export default props => {
   return (
     <li className='friendship-card'>
       <img src={props.friendship.profilePhoto} alt="" className='friendship-card-image'/>
-      <Link to={`/users/${props.friendship.friendId}`} className='user-profile-link'>{props.friendship.firstName} {props.friendship.lastName}</Link>
+      <Link to={`/users/${props.friendship.friendId}`} className='user-profile-link'>{props.friendship.firstName} {props.friendship.lastName}
+      
+      </Link>
     </li>
   )
 }
