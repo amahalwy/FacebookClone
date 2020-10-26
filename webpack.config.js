42547c793d0a2941e616b16c7c60a6f2933a71fd
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-10-25 21:56:15 -0400

    Heroku build

--- webpack.config.js
+++ webpack.config.js
@@ -19,15 +19,6 @@ module.exports = {
       'process.env.REACT_APP_AWS_ACCESS_ID_TOKEN': JSON.stringify(process.env.REACT_APP_AWS_ACCESS_ID_TOKEN),
       'process.env.REACT_APP_S3_BUCKET_TOKEN': JSON.stringify(process.env.REACT_APP_S3_BUCKET_TOKEN)
     }),
-    // new dotenv()
-    // new dotenv({
-    //   path: './.env', // Path to .env file (this is the default)
-    //   safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
-    //   allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
-    //   systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
-    //   silent: true, // hide any errors
-    //   defaults: false // load '.env.defaults' as the default values if empty.
-    // })
   ],
   module: {
     rules: [
