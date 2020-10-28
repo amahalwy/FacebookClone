a9352a6550ca80a199a0b86cb9c18f7d183d8d9d
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-10-28 19:34:43 -0400

    Fix right side of profile body issue

--- frontend/components/user_show/user_show.jsx
+++ frontend/components/user_show/user_show.jsx
@@ -70,12 +70,15 @@ export default props => {
   const hideModal = () => {
     setModal(false);
   };
-
   
   const renderOnMind = () => {
     if (currentUser.id !== user.id && (friendships.every(friendship => friendship.friendId !== currentUser.id) || friendships.length === 0) ) {
       return ''
-    } else if (currentUser.id !== user.id && friendships.every(friendship => friendship.friendId === currentUser.id)) {
+    } else 
+    // debugger
+    if (currentUser.id !== user.id && 
+      friendships.every(friendship => friendship.friendId === currentUser.id) && 
+      friendships.length !== 0) {
       return (
         <div className='post-on-your-mind-container'>
           <div className='post-on-your-mind'>
@@ -89,7 +92,7 @@ export default props => {
           </div>     
         </div>
       )
-    } else {
+    } else if (currentUser.id === user.id) {
       return (
         <div className='post-on-your-mind-container'>
           <div className='post-on-your-mind'>
@@ -163,14 +166,11 @@ export default props => {
         location={props.location}
       />
       <section className='cover-photo-section'>
-
         <div className='cover-photo-container'>
-          
           <img className='cover-photo-img' 
             src={user.coverPhoto} 
             alt="Cover Image" 
           />
-
           <div className='profile-pic-container'> 
             <img 
               className='profile-photo-img'
@@ -185,42 +185,14 @@ export default props => {
 
         <div className='under-cover-info-container' > 
           <div className='under-cover-info'>
-
             <div className='user-name'>{user.firstName} {user.lastName}</div>
-            <div className='user-bio'>
-              {/* <button className='edit-bio-button'>Edit</button> */}
-            </div>
-            
           </div>
-
 
           <div className='under-cover-main'>
             <nav className='under-cover-left-nav'>
-
               <div className='under-cover-timeline'>
                 <span>Timeline</span>
               </div>
-              {/* <div className='under-cover-others under-cover-hover'>
-                <span>About</span>
-              </div> */}
-              {/* <div className='under-cover-others under-cover-hover'>
-                <span className='friends-span'>
-                  <p>Friends</p> 
-                  <p>{friendships.length}</p>
-                </span>
-              </div> */}
-              {/* <div className='under-cover-others under-cover-hover'>
-                <span>Photos</span>
-              </div>
-              <div className='under-cover-others under-cover-hover'>
-                <span>Archive</span>
-              </div>
-              <div className='under-cover-more under-cover-hover'>
-                <span className='more'>More
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-down" className="more-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"></path></svg>
-                </span>
-              </div> */}
-
             </nav>
             
             <nav className='under-cover-right-nav'>  
@@ -230,21 +202,6 @@ export default props => {
                   friendships={friendships}
                 />
               </div>
-              {/* <div className='right-nav-divs'>
-                <button className='right-nav-buttons'>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="eye" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path></svg>
-                </button>
-              </div>
-              <div className='right-nav-divs'>
-                <button className='right-nav-buttons'>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-w-16 fa-search dark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
-                </button>
-              </div>
-              <div className='right-nav-divs'>
-                <button className='right-nav-buttons'>
-                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="ellipsis-h" className="svg-inline--fa fa-w-16 fa-ellipsis-h dark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"></path></svg>
-                </button>
-              </div> */}
             </nav>
           </div>
         </div>
@@ -257,7 +214,6 @@ export default props => {
           <div className='profile-main-left-section'>
             <div className='profile-friends-card'>
               <div className='profile-friend-header'>Friends</div>
-
               <div className='profile-friendship-cards'>
                 {
                   friendships.map(friendship => {
@@ -271,12 +227,12 @@ export default props => {
                   })
                 }
               </div>
-              
             </div>
           </div>
 
           <div className='profile-main-right-section'>
             {renderOnMind()}
+            
             <div className='posts-index-div'>
               <PostIndex
                 history={props.history}
