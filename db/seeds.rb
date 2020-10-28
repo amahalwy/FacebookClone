900bba6a8aa5838a778ba39ec27abd454cb10944
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-10-28 18:25:34 -0400

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ Friendship.destroy_all
 # User.new(first_name, last_name, email, password)
 
 u1 = User.create(first_name: 'Demo', last_name: 'User', email: 'demo', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
-u2 = User.create(first_name: 'Demo2', last_name: 'User2', email: 'duser2', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
-u3 = User.create(first_name: 'Demo3', last_name: 'User3', email: 'duser3', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
+u2 = User.create(first_name: 'Demo', last_name: 'User2', email: 'demo2', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
+u3 = User.create(first_name: 'Demo', last_name: 'User3', email: 'ddemo3', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
 u4 = User.create(first_name: 'John', last_name: 'Appleseed', email: 'jappleseed', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
-u5 = User.create(first_name: 'Sponge', last_name: 'Bob', email: 'sbob', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
-u6 = User.create(first_name: 'Darth', last_name: 'Vader', email: 'dvader', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
+u5 = User.create(first_name: 'Jake', last_name: 'Brighton', email: 'jbrighton', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
+u6 = User.create(first_name: 'Becky', last_name: 'Last', email: 'blast', password: 'demo1234') #, profile_photo: "app/assets/images/facebook_default.png", cover_photo: 'app/assets/images/default-background.jpg')
 
 # Requests
 # FriendRequest.new(requestor_id, receiver_id)
@@ -39,15 +39,17 @@ FriendRequest.create(requestor_id: u4.id, receiver_id: u6.id)
 # Post.new(user_id, body)
 
 p1 = Post.create(user_id: u1.id, owner_id: u1.id, body: "This is your first post. Congratulations, you made it this far!!")
-p2 = Post.create(user_id: u2.id, owner_id: u2.id, body: "This is your first post. What a time to be alive")
-p3 = Post.create(user_id: u5.id, owner_id: u5.id, body: "I got my license out of a cereal box. Don't tell Mrs. Puff")
-p4 = Post.create(user_id: u5.id, owner_id: u5.id, body: "One patty coming right up!")
-p5 = Post.create(user_id: u1.id, owner_id: u6.id, body: "The force is weak on this site. Can I suffocate the creator?")
+p2 = Post.create(user_id: u2.id, owner_id: u2.id, body: "This is your first post. Play around with the site.")
+p3 = Post.create(user_id: u5.id, owner_id: u5.id, body: "Just another demo post. Try adding a picture next post!")
+p4 = Post.create(user_id: u5.id, owner_id: u5.id, body: "Yet another demo post. Try editing this one with the 3 dots top right")
+p5 = Post.create(user_id: u1.id, owner_id: u6.id, body: "A demo post for you to delete. Try it!!!")
+p5 = Post.create(user_id: u1.id, owner_id: u6.id, body: "Another demo post for you to delete. Try it!!!")
+p5 = Post.create(user_id: u1.id, owner_id: u6.id, body: "One more demo post for you to delete.")
 
 # Comments
 # Comment.new(user_id, body, post_id)
 
-Comment.create(user_id: u6.id, body: "Who is this Mrs. Puff? Can I suffocate her too????", post_id: p3.id)
-Comment.create(user_id: u5.id, body: "Do you have to suffocate everything -.- ", post_id: p3.id)
-Comment.create(user_id: u1.id, body: "Sponge, you need to work on your flick of the wrist.", post_id: p4.id)
-Comment.create(user_id: u1.id, body: "I take great offence to that. Please refrain from suffocating things, thanks", post_id: p5.id)
+Comment.create(user_id: u6.id, body: "Some test comments. Try creating your own on this post too!", post_id: p3.id)
+Comment.create(user_id: u5.id, body: "Here's another comment. Can you delete it? Create your own comment and delete that one!!", post_id: p3.id)
+Comment.create(user_id: u1.id, body: "Yes this is another comment, however, try some other functionality. Click on the comment button and watch it focus the text!", post_id: p4.id)
+Comment.create(user_id: u1.id, body: "Here's another comment; try 'liking' this post. If you already liked it, try unliking!", post_id: p5.id)
